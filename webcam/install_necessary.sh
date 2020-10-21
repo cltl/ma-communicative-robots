4df08190a7f8b17f75a2e55b5d3ab92ab008b90d
Taewoon Kim <tae898@gmail.com>
2020-10-21 22:30:10 +0200

    made read friendly

--- webcam/install_necessary.sh
+++ webcam/install_necessary.sh
@@ -1,4 +1,5 @@
-python -m pip install ipywebrtc jupyter
+sudo apt install ffmpeg
+python -m pip install ipywebrtc jupyter av librosa numpy pillow
 python -m jupyter nbextension install --py --symlink --sys-prefix ipywebrtc
 python -m jupyter nbextension enable --py --sys-prefix ipywebrtc
 # python -m jupyter labextension link js
