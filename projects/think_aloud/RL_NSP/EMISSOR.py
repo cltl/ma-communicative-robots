3f782909ae6608f0364a1245be8565fff82c14e0
ThBellucci <40698255+thomas097@users.noreply.github.com>
2021-12-13 17:16:54 +0100

    Upload of project files for RL/NSP method and evaluation

---
+++ projects/think_aloud/RL_NSP/EMISSOR.py
@@ -0,0 +1,95 @@
+""" Filename:     EMISSOR.py
+    Author(s):    Thomas Bellucci
+    Description:  Wrapper around cltl-EMISSOR to incorporate the EMISSOR
+                  platform into the RLChatbot defined in Chatbot.py.
+    Date created: Nov. 11th, 2021
+"""
+
+import os
+import time
+from datetime import datetime
+from random import getrandbits
+import requests
+
+import sys
+sys.path.insert(0, 'cltl-EMISSOR')
+
+from emissor.persistence import ScenarioStorage
+from emissor.representation.scenario import Modality, TextSignal
+
+
+class EMISSOR:
+    def __init__(self, speaker):
+        """ Sets up EMISSOR and its file structure.
+
+            params
+            str speaker: name of the speaker
+
+            returns: None
+        """
+        # Set location and agents
+        self.__place_id = getrandbits(8)
+        self.__location = requests.get("https://ipinfo.io").json()
+        self.__agent = "Leolani1"
+        self.__speaker = speaker
+        self.__speaker_id = self.__speaker.lower()
+
+        # Name scenario ('-' used as ':' is forbidden in Windows filenames)
+        self.__scenario_id = datetime.today().strftime("%Y-%m-%d-%H-%M-%S")
+
+        # Set up folder structure in root directory
+        root_dir = os.path.dirname(os.path.realpath(__file__))
+        self.__scenario_path = os.path.abspath(os.path.join(root_dir, 'scenarios'))
+
+        if not os.path.exists(self.__scenario_path):
+            os.mkdir(self.__scenario_path)
+            print("Created 'scenarios\' for storing scenarios", self.__scenario_path)
+
+        # Create the scenario folder, the json files and a scenarioStorage and scenario in memory
+        time_id = int(time.time() * 1e3)
+        self.__storage = self.create_scenario(self.__scenario_path, self.__scenario_id)
+        self.__scenario = self.__storage.create_scenario(self.__scenario_id, time_id, None, self.__agent)
+        
+    @staticmethod
+    def create_scenario(scenario_path, scenario_id):
+        """ Creates a scenario to store text signals into.
+
+            params
+            str scenario_path: Absolute path to folder to store the scenario into
+            str scenario_id:   Identifier of current scenario
+
+            returns: ScenarioStorage object
+        """
+        # Create scenario storage
+        storage = ScenarioStorage(scenario_path)
+
+        # Create text signal track in './y-m-d-H-M-S/text'.
+        scenario_path = os.path.abspath(os.path.join(storage.base_path, scenario_id))
+        text_sig_path = os.path.abspath(os.path.join(scenario_path, Modality.TEXT.name.lower()))
+        os.mkdir(scenario_path)
+        os.mkdir(text_sig_path)
+        print(f"Directories for {scenario_id} created in {storage.base_path}")
+
+        return storage
+
+    def add_text_signal(self, text):
+        """ Creates a text signal to be stored in EMISSOR.
+
+            params
+            str text: Text to store in the file structure
+
+            returns: None
+        """
+        # Add timestamped text signal to scenario
+        time_id = int(time.time() * 1e3)
+        signal = TextSignal.for_scenario(self.__scenario.id, time_id, time_id, [], text, [])
+        self.__scenario.append_signal(signal)
+
+    def save(self):
+        """ Writes the EMISSOR file structure to disk.
+
+            returns: None
+        """
+        self.__scenario.scenario.ruler.end = int(time.time() * 1e3)
+        self.__storage.save_scenario(self.__scenario)
+        
---
+++ projects/think_aloud/evaluation/usr.py
@@ -0,0 +1,56 @@
+""" Filename:     USR.py
+    Author(s):    Thomas Bellucci
+    Description:  Re-implementation of the USR dialogue evaluation framework. This implementation is limited
+                  to the MCtx score computed by USR as this metric is most meaningful in the context of the project.
+    Date created: Dec. 4th, 2021
+"""
+
+from transformers import RobertaForSequenceClassification, RobertaTokenizer, RobertaConfig
+import numpy as np
+import torch
+
+
+class USR:
+    def __init__(self, path=None):
+        """ Load pretrained and finetuned RoBERTa model for ctx.
+
+            params
+            str path: path to stored model or None
+
+            returns: None
+        """
+        self.__config = RobertaConfig.from_pretrained('adamlin/usr-topicalchat-ctx')
+        self.__tokenizer = RobertaTokenizer.from_pretrained('adamlin/usr-topicalchat-ctx')
+
+        if path is not None:
+            self.__model = RobertaForSequenceClassification.from_pretrained(path, config=self.__config)
+        else:
+            self.__model = RobertaForSequenceClassification.from_pretrained('adamlin/usr-topicalchat-ctx',
+                                                                            config=self.__config)
+
+        self.__device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+        self.__model.to(self.__device)
+
+    def MCtx(self, context, response):
+        """ Scores an input consisting of a (context, response) pair using RoBERTa.
+
+            params
+            str context:  the context strings
+            sre response: response to the context
+
+            returns: score
+        """
+        # Concatenates and encodes context-response pair
+        input_sequence = context + " \n " + response
+        inputs = self.__tokenizer(input_sequence, return_tensors='pt')
+
+        inputs['input_ids'] = inputs['input_ids'].to(self.__device)
+        inputs['attention_mask'] = inputs['attention_mask'].to(self.__device)
+
+        # Feeds encoded input through network
+        outputs = self.__model(**inputs)
+        logits = outputs.logits.detach().cpu().numpy()
+
+        # Returns the softmax score of the positive class, i.e. P(y=1|context, response)
+        outputs = np.exp(logits) / np.sum(np.exp(logits))
+        return outputs[0][1]
