c00aeb9a52a6c8d954060654d904615baf24e1a7
ThBellucci <40698255+thomas097@users.noreply.github.com>
2021-12-20 13:09:00 +0100

    Add files via upload

--- projects/think_aloud/RL_NSP/utils/chatbot_utils.py
+++ projects/think_aloud/RL_NSP/utils/chatbot_utils.py
@@ -4,44 +4,42 @@
     Date created: Nov. 11th, 2021
 """
 
-
 def capsule_for_query(capsule):
-    """Casefolds the triple stored in a capsule so that entities match
-    with the brain regardless of case.
+    """ Casefolds the triple stored in a capsule so that entities match
+        with the brain regardless of case.
 
-    params
-    dict capsule: capsule containing a triple
+        params
+        dict capsule: capsule containing a triple
 
-    returns: capsule with casefolded triple
+        returns: capsule with casefolded triple
     """
-    for item in ["subject", "predicate", "object"]:
-        if capsule[item]["label"]:
-            capsule[item]["label"] = capsule[item]["label"].lower()
+    for item in ['subject', 'predicate', 'object']:
+        if capsule[item]['label']:
+            capsule[item]['label'] = capsule[item]['label'].lower()
     return capsule
 
-
 def triple_for_capsule(triple):
-    """Helper function to prepare a triple for a capsule.
+    """ Helper function to prepare a triple for a capsule.
 
-    params
-    dict triple: triple with multiple type candidates
+        params
+        dict triple: triple with multiple type candidates
 
-    returns: triple with a single type
+        returns: triple with a single type
     """
     subject_type = []
     object_type = []
     predicate_type = []
 
-    if triple["subject"]["type"]:
-        subject_type = triple["subject"]["type"][0]
-    if triple["predicate"]["type"]:
-        predicate_type = triple["predicate"]["type"][0]
-    if triple["object"]["type"]:
-        object_type = triple["object"]["type"][0]
+    if triple['subject']['type']:
+        subject_type = triple['subject']['type'][0]
+    if triple['predicate']['type']:
+        predicate_type = triple['predicate']['type'][0]
+    if triple['object']['type']:
+        object_type = triple['object']['type'][0]
 
     capsule_triple = {
-        "subject": {"label": triple["subject"]["label"], "type": subject_type},
-        "predicate": {"label": triple["predicate"]["label"], "type": predicate_type},
-        "object": {"label": triple["object"]["label"], "type": object_type},
+        "subject": {'label': triple['subject']['label'], 'type': subject_type},
+        "predicate": {'label': triple['predicate']['label'], 'type': predicate_type},
+        "object": {'label': triple['object']['label'], 'type': object_type},
     }
     return capsule_triple
