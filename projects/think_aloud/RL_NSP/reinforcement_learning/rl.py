32bffc510960d73c8fbcab1c91ddf42eb7f11a6c
ThBellucci <40698255+thomas097@users.noreply.github.com>
2021-12-20 13:36:12 +0100

    Add files via upload

--- projects/think_aloud/RL_NSP/reinforcement_learning/rl.py
+++ projects/think_aloud/RL_NSP/reinforcement_learning/rl.py
@@ -5,34 +5,33 @@
     Date created: Nov. 11th, 2021
 """
 
-import json
 import os
 import random
-
-import matplotlib.pyplot as plt
+import json
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 class UCB:
     def __init__(self, c=2, tmax=1e10):
-        """Initializes an instance of the Upper Confidence Bound
-        (UCB) reinforcement learning algorithm.
+        """ Initializes an instance of the Upper Confidence Bound
+            (UCB) reinforcement learning algorithm.
 
-        params
-        float c:          controls level of exploration
-        float tmax:       number of timesteps in which uncertainty of choices
-                          are taken into account (exploitation when t > tmax)
+            params
+            float c:          controls level of exploration
+            float tmax:       number of timesteps in which uncertainty of choices
+                              are taken into account (exploitation when t > tmax)
+            
+            float t:          timestep
+            float decay:      decay rate of exploration constant c
+            float Q:          stores the estimate of the expected reward for each action
+            float N:          stores the number of updates performed on each action
 
-        float t:          timestep
-        float decay:      decay rate of exploration constant c
-        float Q:          stores the estimate of the expected reward for each action
-        float N:          stores the number of updates performed on each action
-
-        returns: UCB object
+            returns: UCB object
         """
         self.__Q = dict()
         self.__N = dict()
-
+        
         self.__t = 1
         self.__c = c
         self.__decay = c / tmax
@@ -40,76 +39,74 @@ class UCB:
     # Utils
 
     def load(self, filename):
-        """Reads utility values from file.
+        """ Reads utility values from file.
 
-        params
-        str filename: filename of file with utilities.
+            params
+            str filename: filename of file with utilities.
 
-        returns: None
+            returns: None
         """
         if filename is None:
             return
 
-        if not os.path.isfile(filename):  # File exists?
-            print("WARNING %s does not yet exist" % filename)
+        if not os.path.isfile(filename): # File exists?
+            print('WARNING %s does not yet exist' % filename)
             return
-
-        with open(filename, "r") as file:
+        
+        with open(filename, 'r') as file:
             data = json.load(file)
-            self.__c = data["metadata"]["c"]
-            self.__t = data["metadata"]["t"]
-            self.__decay = data["metadata"]["decay"]
+            self.__c = data['metadata']['c']
+            self.__t = data['metadata']['t']
+            self.__decay = data['metadata']['decay']
 
-            for action, values in data["data"].items():
-                self.__Q[action] = values["value"]
-                self.__N[action] = values["count"]
-
+            for action, values in data['data'].items():
+                self.__Q[action] = values['value']
+                self.__N[action] = values['count']
+    
     def save(self, filename):
-        """Writes the value and uncertainty tables to a JSON file.
+        """ Writes the value and uncertainty tables to a JSON file.
 
-        params
-        str filename: filename of the ouput file.
+            params
+            str filename: filename of the ouput file.
 
-        returns: None
+            returns: None
         """
         # Format metadata (c, t, decay) and value estimates as JSON.
-        data = {
-            "metadata": {"c": self.__c, "t": self.__t, "decay": self.__decay},
-            "data": dict(),
-        }
+        data = {'metadata':{'c': self.__c,
+                            't': self.__t,
+                            'decay': self.__decay},
+                'data': dict()}
 
         for action in self.__Q.keys():
             if self.__N[action] > 0:
-                data["data"][action] = {
-                    "value": self.__Q[action],
-                    "count": self.__N[action],
-                    "uncertainty": self.__uncertainty(action),
-                }
+                data['data'][action] = {'value': self.__Q[action],
+                                        'count': self.__N[action],
+                                        'uncertainty': self.__uncertainty(action)}
         # Write to file
-        with open(filename, "w") as file:
+        with open(filename, 'w') as file:
             json.dump(data, file)
 
     # Learning
-
+            
     def __uncertainty(self, action):
-        """Computes the uncertainty associated with the current action
-        as the upper confidence bound of the current average.
+        """ Computes the uncertainty associated with the current action
+            as the upper confidence bound of the current average.
 
-        params
-        str action: an action
+            params
+            str action: an action
 
-        returns:    UCB score of the action
+            returns:    UCB score of the action
         """
         return self.__c * np.sqrt(np.log(self.__t) / self.__N[action])
 
     def select_action(self, actions):
-        """Selects an action from the set of available actions that maximizes
-        the average observed reward, taking into account uncertainty.
+        """ Selects an action from the set of available actions that maximizes
+            the average observed reward, taking into account uncertainty.
 
-        params
-        list actions: List of actions from which to select
+            params
+            list actions: List of actions from which to select
 
-        returns: action
+            returns: action
         """
         action_scores = []
         for action in actions:
@@ -137,14 +134,14 @@ class UCB:
         return selected_action
 
     def update_utility(self, action, reward):
-        """Updates the action-value table (Q) by incrementally updating the
-        reward estimate of the action elements with the observed reward.
+        """ Updates the action-value table (Q) by incrementally updating the
+            reward estimate of the action elements with the observed reward.
 
-        params
-        str action:    selected action (with elements elem that are scored)
-        float reward:  reward obtained after performing the action
+            params
+            str action:    selected action (with elements elem that are scored)
+            float reward:  reward obtained after performing the action
 
-        returns: None
+            returns: None
         """
         # Update value estimates
         for elem in action.split():
@@ -156,21 +153,21 @@ class UCB:
         self.__c = max(self.__c - self.__decay, 0)
 
     # Plotting
-
+        
     def plot(self, max_bars=16):
-        """Plots the value estimates for each action and their associated
-        uncertainties in a bar plot.
+        """ Plots the value estimates for each action and their associated
+            uncertainties in a bar plot.
 
-        params
-        float margin: Margin between bars
+            params
+            float margin: Margin between bars
 
-        returns: None
+            returns: None
         """
         total_rewards = sum(self.__N.values())  # empty value table?
         if total_rewards == 0:
             print("WARNING Cannot plot empty value table")
             return
-
+        
         # Estimate value/uncertainty of actions
         a, q, u = [], [], []
         for action in sorted(list(self.__Q.keys())):
@@ -189,13 +186,13 @@ class UCB:
         # Draw barplots for U and Q
         fig = plt.figure(figsize=(10, 5), tight_layout=True)
         plt.suptitle("$t=${}, $c=${}".format(self.__t, round(self.__c, 3)))
-
+        
         plt.subplot(1, 2, 1)
         plt.ylabel("$Uncertainty$ $(U)$")
         plt.xlabel("$Actions$ $(a)$")
         plt.xticks(range(len(a)), a, rotation=45, ha="right")
         plt.bar(range(len(a)), u)
-
+        
         plt.subplot(1, 2, 2)
         plt.ylabel("$Value$ $(Q)$")
         plt.xlabel("$Actions$ $(a)$")
